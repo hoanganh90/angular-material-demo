e030b918886ff23169c08f5495455cc181f91f96
Anh <anh.comdr@gmail.com>
2019-12-29 22:37:26 +0200

    Radio button
    Must include MatRadioModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { NoopAnimationsModule} from '@angular/platform-browser/animations'
-import { MatCheckboxModule } from '@angular/material'
+import { MatCheckboxModule, MatRadioModule } from '@angular/material'
 @NgModule({
   declarations: [
     AppComponent
@@ -12,7 +12,8 @@ import { MatCheckboxModule } from '@angular/material'
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    MatCheckboxModule
+    MatCheckboxModule,
+    MatRadioModule
   ],
   providers: [],
   bootstrap: [AppComponent]
