c643ba7a536af0f591212f0fa71628e690f276db
Anh <anh.comdr@gmail.com>
2020-01-01 17:52:56 +0200

    Inputs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations'
 import { NoopAnimationsModule} from '@angular/platform-browser/animations'
-import { MatCheckboxModule, MatRadioModule, MatSelectModule } from '@angular/material'
+import { MatCheckboxModule, MatRadioModule, MatSelectModule, MatInputModule } from '@angular/material'
 import { FormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
@@ -16,7 +16,8 @@ import { FormsModule } from '@angular/forms';
     MatCheckboxModule,
     MatRadioModule,
     MatSelectModule,
-    FormsModule
+    FormsModule,
+    MatInputModule
   ],
   providers: [],
   bootstrap: [AppComponent]
